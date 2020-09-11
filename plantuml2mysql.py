1a4de83f5d23b9e51eab812b18e475b6901445fb
liuchun2000 <liuchun2000@aliyun.com>
2020-09-11 15:58:52 +0800

    support line use -- comment

--- plantuml2mysql.py
+++ plantuml2mysql.py
@@ -25,7 +25,7 @@ def print_usage():
 
 def main():
     # Check arguments (exactly 1 + 2):
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print_usage()
         sys.exit()
     try: # Avoid exception on STDOUT
@@ -34,81 +34,102 @@ def main():
     except:
         print("Cannot open file: '" + sys.argv[1] + "'")
         sys.exit()
-    # Add information for future self ;-)
-    print("# Database created on", time.strftime('%d/%m/%y %H:%M',time.localtime()), "from", sys.argv[1])
-    print("CREATE DATABASE %s CHARACTER SET = utf8mb4 COLLATE = %s;" % (sys.argv[2], CHARSET))
-    print("USE %s;\n" % sys.argv[2])
-    uml = False; table = False; field = False
-    pk = False; idx = False
-    primary = []; index = ""
-    for l in data:
-        l = l.strip()
-        if not l:
-            continue
-        if l == "```plantuml":
-            uml = True
-            continue
-        if not uml:
-            continue
-
-        comment = l.split("\'")
-        i = comment[0].split()
-        fname = i[0]
-        if fname == ".." or fname == "__": # Separators in table definition
-            continue
-        pk = False; idx = False
-        if fname[0] in ("+", "#"):
-            if fname[0] == "#":
-                pk = True
-            else:
-                idx = True
-            fname = fname[1:]
-        columnComment = ""
-        if field and len(comment)>1:
-            columnComment = comment[1]
-        if l == "```":
-            uml = False
-            continue
-        if not uml:
-             continue
-        if l.startswith("entity"):
-            table = True; field = False
+    try: #
+        with open(sys.argv[3],'w+', encoding='UTF-8') as dest:
+            # Add information for future self ;-)
+            dest.writelines(["# Database created on", time.strftime('%d/%m/%y %H:%M',time.localtime()), "from", sys.argv[1],"\n"])
+            dest.writelines(["CREATE DATABASE %s CHARACTER SET = utf8mb4 COLLATE = %s;\n" % (sys.argv[2], CHARSET)])
+            dest.writelines(["USE %s;\n" % sys.argv[2]])
+            #print("# Database created on", time.strftime('%d/%m/%y %H:%M',time.localtime()), "from", sys.argv[1])
+            #print("CREATE DATABASE %s CHARACTER SET = utf8mb4 COLLATE = %s;" % (sys.argv[2], CHARSET))
+            #print("USE %s;\n" % sys.argv[2])
+            uml = False; table = False; field = False
             primary = []; index = ""
-            # Table names are quoted and lower cased to avoid conflict with a mySQL reserved wordpr
-            print("drop table if exists `" +i[1].lower() + "`;")
-            print("CREATE TABLE `" + i[1].lower() + "` (")
-            continue
-        if table and not field:
-            if l == "==": # Seperator after table description
-                field = True
-                continue
-            else:
-                tableComment = l
-        if field and l == "}":
-            table = False; field = False
-            print(" creator          VARCHAR(64)  NULL,")
-            print(" gmt_create       DATETIME     NULL,")
-            print(" updater          VARCHAR(64)  NULL,")
-            print(" gmt_modified     DATETIME     NULL,")
-            print(" PRIMARY KEY (%s)" % ", ".join(primary), end="")
-            if index:
-                print(",\n%s" % index[:-2],)
-                index = ""
-            print(") COMMENT \'%s\';\n" % tableComment.strip())
-            continue
-        if field and l == "#id":
-            print("  %-16s SERIAL," % "id")
-        if field and l != "#id":
-            print("  %-16s %s" % (fname, " ".join(i[1:]).upper()), end=" ")
-            if columnComment: #other description
-                # Avoid conflict with apostrophes (use double quotation marks)
-                print(" COMMENT \'%s\'" % strip_html_tags(columnComment.strip()), end="")
-            print(",")
-        if field and pk:
-            primary.append(fname)
-        if field and idx:
-            index += "  INDEX (%s),\n" % fname
-
+            for l in data:
+                l = l.strip()
+                if not l:
+                    continue
+                if l == "```plantuml":
+                    uml = True
+                    continue
+                if not uml:
+                    continue
+                if "--" in l: # only one --
+                    dest.writelines(l.strip()+"\n")
+                    continue
+                comment = l.split("\'")
+                i = comment[0].split()
+                fname = i[0]
+                if fname == ".." or fname == "__": # Separators in table definition
+                    continue
+                pk = False; idx = False
+                if fname[0] in ("+", "#"):
+                    if fname[0] == "#":
+                        pk = True
+                    else:
+                        idx = True
+                    fname = fname[1:]
+                columnComment = ""
+                if field and len(comment)>1:
+                    columnComment = comment[1]
+                if l == "```":
+                    uml = False
+                    continue
+                if not uml:
+                    continue
+                if l.startswith("entity"):
+                    table = True; field = False
+                    primary = []; index = ""
+                    # Table names are quoted and lower cased to avoid conflict with a mySQL reserved wordpr
+                    dest.writelines(["\ndrop table if exists `" +i[1].lower() + "`;\n"])
+                    #print("drop table if exists `" +i[1].lower() + "`;")
+                    dest.writelines(["CREATE TABLE `" + i[1].lower() + "` (\n"])
+                    #print("CREATE TABLE `" + i[1].lower() + "` (")
+                    continue
+                if table and not field:
+                    if l == "==": # Seperator after table description
+                        field = True
+                        continue
+                    else:
+                        tableComment = l
+                if field and l == "}":
+                    table = False; field = False
+                    dest.writelines(["  creator          VARCHAR(64)  NULL,\n"])
+                    #print(" creator          VARCHAR(64)  NULL,")
+                    dest.writelines(["  gmt_create       DATETIME     NULL,\n"])
+                    #print(" gmt_create       DATETIME     NULL,")
+                    dest.writelines(["  updater          VARCHAR(64)  NULL,\n"])
+                    #print(" updater          VARCHAR(64)  NULL,")
+                    dest.writelines(["  gmt_modified     DATETIME     NULL,\n"])
+                    #print(" gmt_modified     DATETIME     NULL,")
+                    dest.writelines([" PRIMARY KEY (%s)" % ", ".join(primary)])
+                    #print(" PRIMARY KEY (%s)" % ", ".join(primary), end="")
+                    if index:
+                        dest.write(",\n%s\n" % index[:-2],)
+                        #print(",\n%s" % index[:-2],)
+                        index = ""
+                    dest.write(") COMMENT \'%s\';\n" % tableComment.strip())
+                    #print(") COMMENT \'%s\';\n" % tableComment.strip())
+                    continue
+                if field and l == "#id":
+                    dest.write("  %-16s SERIAL," % "id\n")
+                    #print("  %-16s SERIAL," % "id")
+                if field and l != "#id":
+                    dest.writelines("  %-16s %s " % (fname, " ".join(i[1:]).upper()))
+                    #print("  %-16s %s" % (fname, " ".join(i[1:]).upper()), end=" ")
+                    if columnComment: #other description
+                        # Avoid conflict with apostrophes (use double quotation marks)
+                        dest.writelines(" COMMENT \'%s\'" % strip_html_tags(columnComment.strip()))
+                        #print(" COMMENT \'%s\'" % strip_html_tags(columnComment.strip()), end="")
+                    dest.write(",\n")
+                    #print(",")
+                if field and pk:
+                    primary.append(fname)
+                if field and idx:
+                    index += "  INDEX (%s),\n" % fname
+    except:
+        print("Cannot open file: '" + sys.argv[3] + "'")
+        sys.exit()
 
 if __name__ == "__main__":
     main()
